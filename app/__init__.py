f566252b1ee4ca54df0d81a1182c17dca87c7359
ania <ania.ruiva@gmail.com>
2018-02-11 17:57:40 +0100

    all cleaned

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some-super-secret-key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/mydatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 app.config.from_object(__name__)
